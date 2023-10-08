384c7b98ac8905966546db2bfefd674f9fac9be1
semzhyvotov <semzhivotov@gmail.com>
2023-10-08 17:17:26 +0300

    Collections Task 02V

--- collections-task/src/main/java/org/example/Range.java
+++ collections-task/src/main/java/org/example/Range.java
@@ -1,46 +1,58 @@
 package org.example;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
 
-public class Range<T> implements Set<T> {
+public class Range<T extends Comparable<T>> implements Set<T> {
 
     private T start;
-
     private T end;
-
     private Function<T, T> func;
-
-    private Range(T start, T end) {
-        this.start = start;
-        this.end = end;
-    }
+    private Set<T> additionalElements;
+    private Set<T> removedElements;
 
     private Range(T start, T end, Function<T, T> func) {
         this.start = start;
         this.end = end;
         this.func = func;
+        this.additionalElements = new HashSet<>();
+        this.removedElements = new HashSet<>();
     }
 
-    public static <E> Range<E> of(E start, E end) {
-        return new Range<>(start, end);
+    public static <E extends Comparable<E>> Range<E> of(E start, E end) {
+
+        Function<E, E> function;
+
+        if (start instanceof Number && !(start instanceof Float) && !(start instanceof Double)) {
+            function = e -> (E) Integer.valueOf((int) ((Number) e).longValue() + 1);
+        } else {
+            function = e -> (E) Float.valueOf((float) ((Number) e).doubleValue() + 0.1f);
+        }
+        return new Range<>(start, end, function);
     }
 
-    public static <E> Range<E> of(E start, E end, Function<E, E> func) {
+    public static <E extends Comparable<E>> Range<E> of(E start, E end, Function<E, E> func) {
         return new Range<>(start, end, func);
     }
 
     public int size() {
 
+        if (start == null || end == null) {
+            return 0;
+        }
+
         if (start instanceof Number && !(start instanceof Float) && !(start instanceof Double)) {
 
-            return (int) (((Number) end).longValue() + 1 - ((Number) start).longValue());
+            return (int) (((Number) end).longValue() + 1 - ((Number) start).longValue())
+                    + additionalElements.size()
+                    - removedElements.size();
 
         } else if ((start instanceof Float) || (start instanceof Double)) {
 
-            return (int) Math.round(((Number) end).doubleValue() * 10 + 1 - ((Number) start).doubleValue() * 10);
+            return (int) Math.round(((Number) end).doubleValue() * 10 + 1
+                    - ((Number) start).doubleValue() * 10)
+                    + additionalElements.size()
+                    - removedElements.size();
 
         } else {
             int counter = 0;
@@ -49,36 +61,26 @@ public class Range<T> implements Set<T> {
                 counter++;
                 bufStart = func.apply(bufStart);
             }
-            return counter;
+            return counter + additionalElements.size() - removedElements.size();
         }
     }
 
     public boolean isEmpty() {
-        return start.equals(end);
+        return start == null ||
+                end == null ||
+                start.equals(end) ||
+                removedElements.containsAll(Arrays.asList(this.toArray()));
     }
 
     public boolean contains(Object o) {
-
-        if (start instanceof Number && o instanceof Number) {
-
-            if ((!(o instanceof Float || o instanceof Double) && !(start instanceof Float || start instanceof Double))
-                    || (o instanceof Float || o instanceof Double) && (start instanceof Float || start instanceof Double)) {
-
-                return ((Number) start).doubleValue() <= ((Number) o).doubleValue()
-                        && ((Number) o).doubleValue() <= ((Number) end).doubleValue();
-            }
-
-        } else if (o.getClass() == end.getClass()) {
-            T bufStart = start;
-            while (!bufStart.equals(func.apply(end))) {
-                if (o.equals(bufStart)) {
-                    return true;
-                }
-                bufStart = func.apply(bufStart);
-            }
+        try {
+            return (start.compareTo((T) o) <= 0
+                    && end.compareTo((T) o) >= 0
+                    && !removedElements.contains(o))
+                    || additionalElements.contains(o);
+        } catch (ClassCastException | NullPointerException e) {
+            throw new IllegalArgumentException();
         }
-
-        return false;
     }
 
     public Iterator<T> iterator() {
@@ -86,28 +88,37 @@ public class Range<T> implements Set<T> {
 
             private T currentElement = start;
 
+            private final Iterator<T> addIterator = additionalElements.iterator();
 
             @Override
             public boolean hasNext() {
-                if (func!=null){
-                    return !currentElement.equals(func.apply(end));
-                } else if (start instanceof Number && !(start instanceof Float) && !(start instanceof Double)) {
-                    return !currentElement.equals(((Number) end).intValue() + 1);
-                } else {
-                    return !currentElement.equals(((Number) end).floatValue() + 0.1f);
-                }
+                return (!currentElement.equals(func.apply(end))
+                        || addIterator.hasNext())
+                        && !removedElements.containsAll(additionalElements);
             }
 
             @Override
             public T next() {
+
                 T buf = currentElement;
-                if (func!=null){
+
+                T addBuf;
+
+                if (removedElements.contains(buf)) {
                     currentElement = func.apply(currentElement);
-                } else if (start instanceof Number && !(start instanceof Float) && !(start instanceof Double)) {
-                    currentElement = (T) Integer.valueOf(((Number) currentElement).intValue() + 1);
-                } else {
-                    currentElement = (T) Float.valueOf(((Number) currentElement).floatValue() + 0.1f);
+                    return next();
                 }
+
+                if (end.compareTo(buf) < 0) {
+                    addBuf = addIterator.next();
+                    if (removedElements.contains(buf)) {
+                        return next();
+                    }
+                    return addBuf;
+                }
+
+                currentElement = func.apply(currentElement);
+
                 return buf;
             }
         };
@@ -117,19 +128,50 @@ public class Range<T> implements Set<T> {
     }
 
     public Object[] toArray() {
-        return new Object[0];
+
+        Object[] arr = new Object[this.size()];
+        int i = 0;
+
+        for (var element : this) {
+            arr[i] = element;
+            i++;
+        }
+
+        return arr;
     }
 
     public <T1> T1[] toArray(T1[] a) {
-        return null;
+
+        Iterator<T> iterator = this.iterator();
+
+        for (int i = 0; i < a.length; i++) {
+            if (iterator.hasNext()) {
+                try{
+                    a[i] = (T1) iterator.next();
+                }
+                catch (ClassCastException | NullPointerException e) {
+                    throw new IllegalArgumentException();
+                }
+            } else {
+                break;
+            }
+        }
+
+        return a;
     }
 
     public boolean add(T t) {
-        return false;
+        if (!contains(t)) {
+            additionalElements.add(t);
+            removedElements.remove(t);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean remove(Object o) {
-        return false;
+        return contains(o) && removedElements.add((T) o);
     }
 
     public boolean containsAll(Collection<?> c) {
@@ -142,17 +184,44 @@ public class Range<T> implements Set<T> {
     }
 
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        boolean modified = false;
+        for (T e : c)
+            if (add(e))
+                modified = true;
+        return modified;
     }
 
     public boolean retainAll(Collection<?> c) {
-        return false;
+        Objects.requireNonNull(c);
+        boolean modified = false;
+        Iterator<?> it = iterator();
+        while (it.hasNext()) {
+            if (!c.contains(it.next())) {
+                it.remove();
+                modified = true;
+            }
+        }
+        return modified;
     }
 
     public boolean removeAll(Collection<?> c) {
-        return false;
+        Objects.requireNonNull(c);
+        boolean modified = false;
+        for (var m : c) {
+            if (remove(m)) {
+                modified = true;
+            }
+        }
+        return modified;
     }
 
     public void clear() {
+        start = null;
+        end = null;
+        func = null;
+        additionalElements.clear();
+        additionalElements = null;
+        removedElements.clear();
+        removedElements = null;
     }
 }
